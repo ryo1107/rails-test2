e06e2af6398d8ba4736afd7cf570490f47fbf9cb
Ryo Kato <ryo.github.1107@gmail.com>
2020-05-22 16:20:08 +0900

    bootstrapのヘッダー追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,13 @@ if Rails.env == 'development'
     (1..50).each do |i|
         Board.create(name: "ユーザー#{i}", title: "タイトル#{i}", body: "本文#{i}")
     end
+
+    Tag.create([
+        { name: 'Ruby' },
+        { name: 'RoR4' },
+        { name: 'RoR5' },
+        { name: 'Python2' },
+        { name: 'Python3' },
+        { name: 'Django2' }
+    ])
 end
